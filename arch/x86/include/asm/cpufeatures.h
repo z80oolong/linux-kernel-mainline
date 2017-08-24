4c3f074bfa8a512f0a0a8d1690c32ee9608badc5
Tim Chen <tim.c.chen@linux.intel.com>
2017-08-24 09:34:41 -0700

    x86/feature: Enable the x86 feature to control Speculation
    CVE-2017-5715 (Spectre v2 Intel)
    
    cpuid ax=0x7, return rdx bit 26 to indicate presence of this feature
    IA32_SPEC_CTRL (0x48) and IA32_PRED_CMD (0x49)
    IA32_SPEC_CTRL, bit0 – Indirect Branch Restricted Speculation (IBRS)
    IA32_PRED_CMD,  bit0 – Indirect Branch Prediction Barrier (IBPB)
    
    Signed-off-by: Tim Chen <tim.c.chen@linux.intel.com>
    Signed-off-by: Andy Whitcroft <apw@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>
    Signed-off-by: Andy Whitcroft <apw@canonical.com>
    Acked-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -211,6 +211,7 @@
 
 #define X86_FEATURE_MBA			( 7*32+18) /* Memory Bandwidth Allocation */
 #define X86_FEATURE_RSB_CTXSW		( 7*32+19) /* Fill RSB on context switches */
+#define X86_FEATURE_SPEC_CTRL		( 7*32+20) /* Control Speculation Control */
 
 /* Virtualization flags: Linux defined, word 8 */
 #define X86_FEATURE_TPR_SHADOW		( 8*32+ 0) /* Intel TPR Shadow */
