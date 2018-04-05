2695744c5fe8781d0cbb2c0928cadbc75d52ab21
Tyler Hicks <tyhicks@canonical.com>
2018-04-05 07:51:00 +0200

    Revert "x86/mm: Only set IBPB when the new thread cannot ptrace current thread"
    BugLink: https://bugs.launchpad.net/bugs/1759920
    CVE-2017-5715 (Spectre v2 Intel)
    
    This reverts commit 96d520d0fd4994643216f30fe91eea770ba934bc.
    
    Using a ptrace access check in the middle of a task switch was causing
    a hard lockup in some cases when the old task was confined by AppArmor.
    If the AppArmor policy for the the old task didn't allow the task to
    ptrace the new task, AppArmor would attempt to emit an audit message and
    deadlock on the task's pi_lock would occur. The fix is to revert this
    change and go with upstream's implementation that uses the task's
    dumpable state to determine if IBPB should be used.
    
    Signed-off-by: Tyler Hicks <tyhicks@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- arch/x86/mm/tlb.c
+++ arch/x86/mm/tlb.c
@@ -6,7 +6,6 @@
 #include <linux/interrupt.h>
 #include <linux/export.h>
 #include <linux/cpu.h>
-#include <linux/ptrace.h>
 
 #include <asm/tlbflush.h>
 #include <asm/mmu_context.h>
@@ -220,9 +219,7 @@ void switch_mm_irqs_off(struct mm_struct *prev, struct mm_struct *next,
 		u16 new_asid;
 		bool need_flush;
 
-		/* Null tsk means switching to kernel, so that's safe */
-		if (ibpb_inuse && tsk &&
-			___ptrace_may_access(tsk, current, PTRACE_MODE_IBPB))
+		if (ibpb_inuse && boot_cpu_has(X86_FEATURE_SPEC_CTRL))
 			native_wrmsrl(MSR_IA32_PRED_CMD, FEATURE_SET_IBPB);
 
 		if (IS_ENABLED(CONFIG_VMAP_STACK)) {
