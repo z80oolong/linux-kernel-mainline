6d6a64384c3615b5e4cdbee8219678913a290a0f
Heiner Kallweit <hkallweit1@gmail.com>
2018-04-28 22:19:43 +0200

    r8169: remove calls to rtl_set_rx_mode
    BugLink: https://bugs.launchpad.net/bugs/1757422
    
    __dev_open() calls the ndo_set_rx_mode callback anyway, so we don't
    have to do it here too.
    
    Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 82d3ff6dd1994d54fe11714ffd4c696cfcacbea1)
    Signed-off-by: Kai-Heng Feng <kai.heng.feng@canonical.com>

--- drivers/net/ethernet/realtek/r8169.c
+++ drivers/net/ethernet/realtek/r8169.c
@@ -5446,8 +5446,6 @@ static void rtl_hw_start_8169(struct rtl8169_private *tp)
 
 	RTL_W32(tp, RxMissed, 0);
 
-	rtl_set_rx_mode(tp->dev);
-
 	/* no early-rx interrupts */
 	RTL_W16(tp, MultiIntr, RTL_R16(tp, MultiIntr) & 0xf000);
 }
@@ -6362,8 +6360,6 @@ static void rtl_hw_start_8168(struct rtl8169_private *tp)
 
 	RTL_W8(tp, ChipCmd, CmdTxEnb | CmdRxEnb);
 
-	rtl_set_rx_mode(tp->dev);
-
 	RTL_W16(tp, MultiIntr, RTL_R16(tp, MultiIntr) & 0xf000);
 }
 
@@ -6554,8 +6550,6 @@ static void rtl_hw_start_8101(struct rtl8169_private *tp)
 	RTL_W16(tp, IntrMitigate, 0x0000);
 
 	RTL_W8(tp, ChipCmd, CmdTxEnb | CmdRxEnb);
-
-	rtl_set_rx_mode(tp->dev);
 
 	RTL_R8(tp, IntrMask);
 
