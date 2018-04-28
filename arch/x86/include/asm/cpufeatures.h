5372b0e164a585f68ebf2ff93f4a6a403aae7429
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2018-04-28 22:34:17 +0200

    x86/cpufeatures: Add X86_FEATURE_RDS
    Add the CPU feature bit CPUID.7.0.EDX[31] which indicates whether the CPU
    supports Reduced Data Speculation.
    
    [ tglx: Split it out from a later patch ]
    
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Ingo Molnar <mingo@kernel.org>
    
    CVE-2018-3639 (x86)
    
    Signed-off-by: Tyler Hicks <tyhicks@canonical.com>
    [smb: backport add scattered cpuid info for RDS]
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -213,6 +213,7 @@
 #define X86_FEATURE_RSB_CTXSW		( 7*32+19) /* Fill RSB on context switches */
 #define X86_FEATURE_SPEC_CTRL		( 7*32+20) /* Control Speculation Control */
 #define X86_FEATURE_ARCH_CAPABILITIES	( 7*32+21) /* IA32_ARCH_CAPABILITIES MSR (Intel) */
+#define X86_FEATURE_RDS			( 7*32+22) /* Reduced Data Speculation */
 
 /* Virtualization flags: Linux defined, word 8 */
 #define X86_FEATURE_TPR_SHADOW		( 8*32+ 0) /* Intel TPR Shadow */
