90e598b4d1c45049d77019b8c2297f8b1686ff38
Johannes Berg <johannes.berg@intel.com>
2019-05-20 14:49:56 +0200

    iwlwifi: mvm: delay GTK setting in FW in AP mode
    BugLink: https://bugs.launchpad.net/bugs/1840846
    
    commit c56e00a3feaee2b46b7d33875fb7f52efd30241f upstream.
    
    In AP (and IBSS) mode, we can only set GTKs to firmware after we have
    sent down the multicast station, but this we can only do after we've
    enabled beaconing, etc.
    
    However, during rfkill exit, hostapd will configure the keys before
    starting the AP, and cfg80211/mac80211 accept it happily.
    
    On earlier devices, this didn't bother us as GTK TX wasn't really
    handled in firmware, we just put the key material into the TX cmd
    and thus it only mattered when we actually transmitted a frame.
    
    On newer devices, however, the firmware needs to track all of this
    and that doesn't work if we add the key before the (multicast) sta
    it belongs to.
    
    To fix this, keep a list of keys to add during AP enable, and call
    the function there.
    
    Cc: stable@vger.kernel.org
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- drivers/net/wireless/intel/iwlwifi/mvm/mvm.h
+++ drivers/net/wireless/intel/iwlwifi/mvm/mvm.h
@@ -474,6 +474,9 @@ struct iwl_mvm_vif {
 
 	struct iwl_probe_resp_data __rcu *probe_resp_data;
 	struct ieee80211_key_conf *ap_wep_key;
+
+	/* we can only have 2 GTK + 2 IGTK active at a time */
+	struct ieee80211_key_conf *ap_early_keys[4];
 };
 
 static inline struct iwl_mvm_vif *
