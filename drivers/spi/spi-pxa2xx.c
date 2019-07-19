344b39d5b0e29bdbf5d6cecd5fbf33a095943dbb
Lubomir Rintel <lkundrak@v3.sk>
2019-07-19 14:27:13 +0200

    spi: pxa2xx: Balance runtime PM enable/disable on error
    BugLink: https://bugs.launchpad.net/bugs/1842128
    
    [ Upstream commit 1274204542f683e1d8491ebe9cc86284d5a8ebcc ]
    
    Don't undo the PM initialization if we error out before we managed to
    initialize it. The call to pm_runtime_disable() without being preceded
    by pm_runtime_enable() would disturb the balance of the Force.
    
    In practice, this happens if we fail to allocate any of the GPIOS ("cs",
    "ready") due to -EPROBE_DEFER because we're getting probled before the
    GPIO driver.
    
    Signed-off-by: Lubomir Rintel <lkundrak@v3.sk>
    Link: https://lore.kernel.org/r/20190719122713.3444318-1-lkundrak@v3.sk
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- drivers/spi/spi-pxa2xx.c
+++ drivers/spi/spi-pxa2xx.c
@@ -1818,14 +1818,16 @@ static int pxa2xx_spi_probe(struct platform_device *pdev)
 	status = devm_spi_register_controller(&pdev->dev, master);
 	if (status != 0) {
 		dev_err(&pdev->dev, "problem registering spi master\n");
-		goto out_error_clock_enabled;
+		goto out_error_pm_runtime_enabled;
 	}
 
 	return status;
 
-out_error_clock_enabled:
+out_error_pm_runtime_enabled:
 	pm_runtime_put_noidle(&pdev->dev);
 	pm_runtime_disable(&pdev->dev);
+
+out_error_clock_enabled:
 	clk_disable_unprepare(ssp->clk);
 
 out_error_dma_irq_alloc:
