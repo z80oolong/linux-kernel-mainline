d942b0095443e4110a4713f2c7382a2a72182e65
Paolo Bonzini <pbonzini@redhat.com>
2019-09-30 18:48:44 +0200

    kvm: x86, powerpc: do not allow clearing largepages debugfs entry
    The largepages debugfs entry is incremented/decremented as shadow
    pages are created or destroyed.  Clearing it will result in an
    underflow, which is harmless to KVM but ugly (and could be
    misinterpreted by tools that use debugfs information), so make
    this particular statistic read-only.
    
    Cc: kvm-ppc@vger.kernel.org
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    
    CVE-2018-12207
    
    (backported from commit 833b45de69a6016c4b0cebe6765d526a31a81580)
    [tyhicks: Backport to 5.0
     - largepages_* are not present in the debugfs_entries list in book3s.c
     - VM_STAT() does not exist in book3s.c]
    Signed-off-by: Tyler Hicks <tyhicks@canonical.com>
    
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -1041,6 +1041,7 @@ enum kvm_stat_kind {
 
 struct kvm_stat_data {
 	int offset;
+	int mode;
 	struct kvm *kvm;
 };
 
@@ -1048,6 +1049,7 @@ struct kvm_stats_debugfs_item {
 	const char *name;
 	int offset;
 	enum kvm_stat_kind kind;
+	int mode;
 };
 extern struct kvm_stats_debugfs_item debugfs_entries[];
 extern struct dentry *kvm_debugfs_dir;
