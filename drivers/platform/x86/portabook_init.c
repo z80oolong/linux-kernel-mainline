2f2fb7ffc53f64f71508ba3859b781753c589d67
NAKATSUKA, Yukitaka <zool@zool.jpn.org>
2019-12-13 17:38:51 +0900

    Add to kernel KingJim XMC10 extra kernel module.
    Add codes in the https://github.com/amuramatsu/portabook_ext.git
    
    Fix drivers/platform/x86/Kconfig

---
+++ drivers/platform/x86/portabook_init.c
@@ -0,0 +1,67 @@
+/*
+ * portabook_init.c - Portabook extra module
+ * Copyright (C) 2016  MURAMATSU Atsushi <amura@tomato.sakura.ne.jp>
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or (at
+ *  your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_DESCRIPTION("Portabook extra Module");
+MODULE_AUTHOR("MURAMATSU Atsushi <amura@tomato.sakura.ne.jp>");
+MODULE_LICENSE("GPL");
+
+#ifdef CONFIG_PORTABOOK_EXT_BACKLIGHT
+extern int portabook_backlight_init(void);
+extern void portabook_backlight_cleanup(void);
+#endif
+#ifdef CONFIG_PORTABOOK_EXT_BATTERY
+extern int portabook_battery_init(void);
+extern void portabook_battery_cleanup(void);
+#endif
+
+static int
+portabook_ext_init_module(void)
+{
+    int error = 0;
+
+    printk("portabook_ext is loaded!\n");
+#ifdef CONFIG_PORTABOOK_EXT_BACKLIGHT
+    error = portabook_backlight_init();
+    if (error)
+	return error;
+#endif
+#ifdef CONFIG_PORTABOOK_EXT_BATTERY
+    error = portabook_battery_init();
+    if (error)
+	return error;
+#endif
+    return 0;
+}
+
+static void portabook_ext_cleanup_module(void)
+{
+#ifdef CONFIG_PORTABOOK_EXT_BATTERY
+    portabook_battery_cleanup();
+#endif
+#ifdef CONFIG_PORTABOOK_EXT_BACKLIGHT
+    portabook_backlight_cleanup();
+#endif
+    printk("portabook_ext is unloaded!\n");
+}
+
+module_init(portabook_ext_init_module);
+module_exit(portabook_ext_cleanup_module);
